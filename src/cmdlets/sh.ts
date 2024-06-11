8532444190d1f0f8ecabe42838bb3ae9be6f245c
Your Name <you@example.com>
2024-06-11 19:19:03 +0100

    More refactor

--- src/cmdlets/sh.ts
+++ src/cmdlets/sh.ts
@@ -14,7 +14,7 @@ const STDERR_FILENO: number = 2;
 const READ_SIZE: number = 4096;
 const WNOHANG: number = 1;
 
-type Pipes = { readFd: number; writeFd: number };
+type Pipe = { readFd: number; writeFd: number };
 
 const USAGE: string = `Usage: fd
 sh - run a shell
@@ -88,155 +88,26 @@ export class ShCmdLet extends CmdLet {
       throw new Error(`failed to getenv("SHELL"), errno: ${getenvErrno}`);
 
     Output.writeln(`SHELL: ${shellVar.readUtf8String()}`, true);
+    const childPipe = this.createPipe();
+    const parentPipe = this.createPipe();
 
-    const { readFd: toChildRead, writeFd: toChildWrite } = this.createPipe();
-    const { readFd: toParentRead, writeFd: toParentWrite } = this.createPipe();
-
-    const { value: forkRet, errno: forkErrno } =
+    const { value: childPid, errno: forkErrno } =
       this.fnFork() as UnixSystemFunctionResult<number>;
-    if (forkRet < 0) throw new Error(`failed to fork, errno: ${forkErrno}`);
+    if (childPid < 0) throw new Error(`failed to fork, errno: ${forkErrno}`);
 
-    if (forkRet === 0) {
-      // child
+    if (childPid === 0) {
       try {
-        const { value: closeChildRet, errno: closeChildErrno } = this.fnClose(
-          toChildWrite,
-        ) as UnixSystemFunctionResult<number>;
-        if (closeChildRet !== 0)
-          throw new Error(`failed to close(child), errno: ${closeChildErrno}`);
-
-        const { value: closeParentRet, errno: closeParentErrno } = this.fnClose(
-          toParentRead,
-        ) as UnixSystemFunctionResult<number>;
-        if (closeParentRet !== 0)
-          throw new Error(
-            `failed to close(parent), errno: ${closeParentErrno}`,
-          );
-
-        const { value: dup2InRet, errno: dup2InErrno } = this.fnDup2(
-          toChildRead,
-          STDIN_FILENO,
-        ) as UnixSystemFunctionResult<number>;
-        if (dup2InRet !== STDIN_FILENO)
-          throw new Error(`failed to dup2(stdin), errno: ${dup2InErrno}`);
-
-        const { value: dup2OutRet, errno: dup2OutErrno } = this.fnDup2(
-          toParentWrite,
-          STDOUT_FILENO,
-        ) as UnixSystemFunctionResult<number>;
-        if (dup2OutRet !== STDOUT_FILENO)
-          throw new Error(`failed to dup2(stdout), errno: ${dup2OutErrno}`);
-
-        const { value: dup2ErrRet, errno: dup2ErrErrno } = this.fnDup2(
-          toParentWrite,
-          STDERR_FILENO,
-        ) as UnixSystemFunctionResult<number>;
-        if (dup2ErrRet !== STDERR_FILENO)
-          throw new Error(`failed to dup2(stderr), errno: ${dup2ErrErrno}`);
-
-        const cmd = '/usr/bin/ping';
-        const cmdPtr = Memory.allocUtf8String(cmd);
-        const args = [cmd, '-c3', 'localhost'];
-        const argPtrs: NativePointer[] = Array.from(args).map(arg =>
-          Memory.allocUtf8String(arg),
-        );
-        argPtrs.push(ptr(0));
-        const argv = Memory.alloc(Process.pointerSize * argPtrs.length);
-        for (const [idx, ptr] of argPtrs.entries()) {
-          argv.add(Process.pointerSize * idx).writePointer(ptr);
-        }
-
-        const { value: execvRet, errno: execvErrno } = this.fnExecV(
-          cmdPtr,
-          argv,
-        ) as UnixSystemFunctionResult<number>;
-        if (execvRet !== 0)
-          throw new Error(`failed to execv, errno: ${execvErrno}`);
-      } catch (error) {
-        const output = new UnixOutputStream(toParentWrite);
-        if (error instanceof Error) {
-          output.write(Format.toByteArray(`ERROR: ${error.message}`));
-          output.write(Format.toByteArray(`${error.stack}`));
-        } else {
-          output.write(Format.toByteArray('Unknown error'));
-        }
-        output.close();
+        this.runChild(childPipe, parentPipe);
       } finally {
         this.fnExit(1);
       }
     } else {
-      // parent
-      try {
-        Output.writeln(`child pid: ${forkRet}`, true);
-        const { value: closeChildRet, errno: closeChildErrno } = this.fnClose(
-          toChildRead,
-        ) as UnixSystemFunctionResult<number>;
-        if (closeChildRet !== 0)
-          throw new Error(`failed to close(child), errno: ${closeChildErrno}`);
-
-        const { value: closeParentRet, errno: closeParentErrno } = this.fnClose(
-          toParentWrite,
-        ) as UnixSystemFunctionResult<number>;
-        if (closeParentRet !== 0)
-          throw new Error(
-            `failed to close(parent), errno: ${closeParentErrno}`,
-          );
-
-        const input = new UnixInputStream(toParentRead, { autoClose: true });
-
-        const output = new UnixOutputStream(toChildWrite, { autoClose: true });
-
-        const onRaw: InputInterceptRaw = {
-          addRaw(raw: string) {
-            output.write(Format.toByteArray(raw));
-          },
-          abort() {},
-        };
-
-        Input.setInterceptRaw(onRaw);
-
-        Output.writeln(`reading pid: ${forkRet}`, true);
-
-        for (
-          let buf = await input.read(READ_SIZE);
-          buf.byteLength !== 0;
-          buf = await input.read(READ_SIZE)
-        ) {
-          const str: string = Format.toTextString(buf);
-          Output.write(str);
-        }
-
-        Output.writeln(`waiting pid: ${forkRet}`, true);
-
-        const pStatus = Memory.alloc(INT_SIZE);
-
-        const { value: waitRet, errno: waitErrno } = this.fnWaitPid(
-          forkRet,
-          pStatus,
-          WNOHANG,
-        ) as UnixSystemFunctionResult<number>;
-        if (waitRet < 0)
-          throw new Error(`failed to waitpid ${waitRet}, errno: ${waitErrno}`);
-
-        if (waitRet !== forkRet)
-          throw new Error(`failed to waitpid ${waitRet}`);
-
-        const status = pStatus.readInt();
-        if (ShCmdLet.wifExited(status)) {
-          const exitStatus = ShCmdLet.wExitStatus(status);
-          Output.writeln(`exit status: ${exitStatus}`);
-        } else if (ShCmdLet.wifSignalled(status)) {
-          const termSig = ShCmdLet.wTermSig(status);
-          Output.writeln(`terminated by signal: ${termSig}`);
-        }
-      } finally {
-        Input.setInterceptRaw(null);
-      }
+      await this.runParent(childPid, childPipe, parentPipe);
     }
     return Var.ZERO;
   }
 
-  private createPipe(): Pipes {
+  private createPipe(): Pipe {
     if (this.fnPipe === null)
       throw new Error('failed to find necessary native functions');
     const pipes = Memory.alloc(INT_SIZE * 2);
@@ -247,6 +118,158 @@ export class ShCmdLet extends CmdLet {
     const readFd = pipes.add(PIPE_READ_OFFSET).readInt();
     const writeFd = pipes.add(PIPE_WRITE_OFFSET).readInt();
     return { readFd, writeFd };
+  }
+
+  private runChild(childPipe: Pipe, parentPipe: Pipe) {
+    const { readFd: toChildReadFd, writeFd: toChildWriteFd } = childPipe;
+    const { readFd: toParentReadFd, writeFd: toParentWriteFd } = parentPipe;
+    try {
+      if (
+        this.fnGetEnv == null ||
+        this.fnClose === null ||
+        this.fnDup2 === null ||
+        this.fnExecV == null ||
+        this.fnExit == null
+      )
+        throw new Error('failed to find necessary native functions');
+
+      const { value: closeChildRet, errno: closeChildErrno } = this.fnClose(
+        toChildWriteFd,
+      ) as UnixSystemFunctionResult<number>;
+      if (closeChildRet !== 0)
+        throw new Error(`failed to close(child), errno: ${closeChildErrno}`);
+
+      const { value: closeParentRet, errno: closeParentErrno } = this.fnClose(
+        toParentReadFd,
+      ) as UnixSystemFunctionResult<number>;
+      if (closeParentRet !== 0)
+        throw new Error(`failed to close(parent), errno: ${closeParentErrno}`);
+
+      const { value: dup2InRet, errno: dup2InErrno } = this.fnDup2(
+        toChildReadFd,
+        STDIN_FILENO,
+      ) as UnixSystemFunctionResult<number>;
+      if (dup2InRet !== STDIN_FILENO)
+        throw new Error(`failed to dup2(stdin), errno: ${dup2InErrno}`);
+
+      const { value: dup2OutRet, errno: dup2OutErrno } = this.fnDup2(
+        toParentWriteFd,
+        STDOUT_FILENO,
+      ) as UnixSystemFunctionResult<number>;
+      if (dup2OutRet !== STDOUT_FILENO)
+        throw new Error(`failed to dup2(stdout), errno: ${dup2OutErrno}`);
+
+      const { value: dup2ErrRet, errno: dup2ErrErrno } = this.fnDup2(
+        toParentWriteFd,
+        STDERR_FILENO,
+      ) as UnixSystemFunctionResult<number>;
+      if (dup2ErrRet !== STDERR_FILENO)
+        throw new Error(`failed to dup2(stderr), errno: ${dup2ErrErrno}`);
+
+      const cmd = '/usr/bin/ping';
+      const cmdPtr = Memory.allocUtf8String(cmd);
+      const args = [cmd, '-c3', 'localhost'];
+      const argPtrs: NativePointer[] = Array.from(args).map(arg =>
+        Memory.allocUtf8String(arg),
+      );
+      argPtrs.push(ptr(0));
+      const argv = Memory.alloc(Process.pointerSize * argPtrs.length);
+      for (const [idx, ptr] of argPtrs.entries()) {
+        argv.add(Process.pointerSize * idx).writePointer(ptr);
+      }
+
+      const { value: execvRet, errno: execvErrno } = this.fnExecV(
+        cmdPtr,
+        argv,
+      ) as UnixSystemFunctionResult<number>;
+      if (execvRet !== 0)
+        throw new Error(`failed to execv, errno: ${execvErrno}`);
+    } catch (error) {
+      const output = new UnixOutputStream(toParentWriteFd);
+      if (error instanceof Error) {
+        output.write(Format.toByteArray(`ERROR: ${error.message}`));
+        output.write(Format.toByteArray(`${error.stack}`));
+      } else {
+        output.write(Format.toByteArray('Unknown error'));
+      }
+      output.close();
+    }
+  }
+
+  private async runParent(childPid: number, childPipe: Pipe, parentPipe: Pipe) {
+    const { readFd: toChildReadFd, writeFd: toChildWriteFd } = childPipe;
+    const { readFd: toParentReadFd, writeFd: toParentWriteFd } = parentPipe;
+    try {
+      if (
+        this.fnClose === null ||
+        this.fnWaitPid === null
+      )
+        throw new Error('failed to find necessary native functions');
+
+      Output.writeln(`child pid: ${childPid}`, true);
+      const { value: closeChildRet, errno: closeChildErrno } = this.fnClose(
+        toChildReadFd,
+      ) as UnixSystemFunctionResult<number>;
+      if (closeChildRet !== 0)
+        throw new Error(`failed to close(child), errno: ${closeChildErrno}`);
+
+      const { value: closeParentRet, errno: closeParentErrno } = this.fnClose(
+        toParentWriteFd,
+      ) as UnixSystemFunctionResult<number>;
+      if (closeParentRet !== 0)
+        throw new Error(`failed to close(parent), errno: ${closeParentErrno}`);
+
+      const input = new UnixInputStream(toParentReadFd, { autoClose: true });
+
+      const output = new UnixOutputStream(toChildWriteFd, {
+        autoClose: true,
+      });
+
+      const onRaw: InputInterceptRaw = {
+        addRaw(raw: string) {
+          output.write(Format.toByteArray(raw));
+        },
+        abort() {},
+      };
+
+      Input.setInterceptRaw(onRaw);
+
+      Output.writeln(`reading pid: ${childPid}`, true);
+
+      for (
+        let buf = await input.read(READ_SIZE);
+        buf.byteLength !== 0;
+        buf = await input.read(READ_SIZE)
+      ) {
+        const str: string = Format.toTextString(buf);
+        Output.write(str);
+      }
+
+      Output.writeln(`waiting pid: ${childPid}`, true);
+
+      const pStatus = Memory.alloc(INT_SIZE);
+
+      const { value: waitRet, errno: waitErrno } = this.fnWaitPid(
+        childPid,
+        pStatus,
+        WNOHANG,
+      ) as UnixSystemFunctionResult<number>;
+      if (waitRet < 0)
+        throw new Error(`failed to waitpid ${waitRet}, errno: ${waitErrno}`);
+
+      if (waitRet !== childPid) throw new Error(`failed to waitpid ${waitRet}`);
+
+      const status = pStatus.readInt();
+      if (ShCmdLet.wifExited(status)) {
+        const exitStatus = ShCmdLet.wExitStatus(status);
+        Output.writeln(`exit status: ${exitStatus}`);
+      } else if (ShCmdLet.wifSignalled(status)) {
+        const termSig = ShCmdLet.wTermSig(status);
+        Output.writeln(`terminated by signal: ${termSig}`);
+      }
+    } finally {
+      Input.setInterceptRaw(null);
+    }
   }
 
   private static wifExited(status: number): boolean {
